2cf1b31cb50b64f29fce0ab3fe8b50e71b602719
Nod59077 <nod59077@gmail.com>
2024-10-04 16:32:47 +0200

    update

--- EV3 client/client.py
+++ EV3 client/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import requests
+import requests                                 #import all necessary libraries
 from legoFunctions import *
 from commandFunction import command
 import scanner
